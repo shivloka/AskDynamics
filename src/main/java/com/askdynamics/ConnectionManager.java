326778e419cc654899e57ecd8972ce427c710974
shashankdevan <shashankdevan@gmail.com>
2015-09-17 11:11:50 -0700

    ConnectionManager is now Singleton

--- src/main/java/com/askdynamics/ConnectionManager.java
+++ src/main/java/com/askdynamics/ConnectionManager.java
@@ -4,8 +4,22 @@ import com.mongodb.MongoClient;
 
 public class ConnectionManager {
 
-    public MongoClient getClient() {
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
+    private static volatile ConnectionManager INSTANCE;
+    private MongoClient mongoClient;
+
+    private ConnectionManager(){
+        mongoClient = new MongoClient("localhost", 27017);
+    }
+    public static ConnectionManager getInstance(){
+        if(INSTANCE == null){
+            synchronized (ConnectionManager.class){
+                if(INSTANCE == null)
+                    INSTANCE = new ConnectionManager();
+            }
+        }
+        return INSTANCE;
+    }
+    public MongoClient getMongoClient() {
         return mongoClient;
     }
 }

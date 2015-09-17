78a4ffa1b0a98ddac47f149e14de9a80635422fd
adwait.bhandare <adwait.bhandare@appdynamics.com>
2015-09-17 16:39:23 -0700

    Adding Util layer

--- src/main/java/com/askdynamics/Util/SearchUtil.java
+++ src/main/java/com/askdynamics/Util/SearchUtil.java
@@ -1,7 +1,42 @@
 package com.askdynamics.Util;
 
+import com.askdynamics.ConnectionManager;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by adwait.bhandare on 9/17/15.
  */
 public class SearchUtil {
+
+    private MongoDatabase getDatabaseObject() {
+        MongoClient dbClient = ConnectionManager.getInstance().getMongoClient();
+        return dbClient.getDatabase("askdynamics");
+    }
+
+    public List<DBObject> search(List<String> searchlist,){
+        Iterator<String> strIterator = searchlist.iterator();
+        MongoDatabase db = getDatabaseObject();
+
+       while(strIterator.hasNext()){
+            String pattern = strIterator.next();
+            DBCursor cursor=db.getCollection("Question").find( {
+                    "or": [
+            { title: { $regex: pattern, "options": 'simx' } },
+            { body: { $regex: pattern, 'options': 'simx' } },
+            { tags: { $regex: pattern, $options: 'simx' } },
+            { comment: { $regex: pattern, $options: 'simx' } }
+            ]
+            } );
+            cursor.Collection.distinct("_id");
+        }
+
+
+    return null;
+    }
 }
--- src/main/java/com/askdynamics/Util/StringUtil.java
+++ src/main/java/com/askdynamics/Util/StringUtil.java
@@ -1,7 +1,60 @@
 package com.askdynamics.Util;
 
-/**
- * Created by adwait.bhandare on 9/17/15.
- */
-public class StringUtil {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class StringUtil{
+
+    public static List<String> getAllCombination(String searchStr){
+
+        if(searchStr==null){
+            return null;
+        }
+
+        List<String> combinations = new ArrayList<String>();
+        getConfigurations(0, searchStr.split(" "), combinations, new StringBuffer());
+
+        Comparator<String> StringComparator = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.length() - o1.length();
+            }
+        };
+        Collections.sort(combinations, StringComparator);
+
+        for (String str : combinations){
+            System.out.println(str);
+        }
+        //  return arrangeCombinations(combinations);
+        return combinations;
+
+    }
+
+
+
+
+    private static void getConfigurations (int index, String[] input, List<String> output, StringBuffer buffer) {
+        //  for (index; index < )
+        if (index >= input.length)
+            return;
+
+        for (int cnt = index; cnt < input.length; cnt++){
+            if (cnt > index)
+                buffer.append(" ");
+
+            buffer.append(input[cnt]);
+
+            output.add(buffer.toString().toLowerCase());
+            //System.out.println(buffer.toString().toLowerCase());
+        }
+
+        getConfigurations(index+1, input, output, new StringBuffer());
+    }
+
+    public static void main(String[] args) {
+        getAllCombination("Agent Configuraion Setup");
+    }
 }
